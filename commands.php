b6d5eca1b1d1875bdfc6f2464629efb39a20e38d
adiyansyah <adiyansyahdwiputra@gmail.com>
2019-08-15 15:05:24 +0700

    Completed calculator v0.6 release

---
+++ commands.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+   \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\DivideCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\PowCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\HistoryCommand::class,
+   \Jakmall\Recruitment\Calculator\Commands\ClearHistoryCommand::class,
+];
